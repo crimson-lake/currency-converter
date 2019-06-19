addee05d7623606ae8af1d7af6af9722b4effb33
crimson-lake <sandra.zielinska@protonmail.com>
2019-06-19 19:12:33 +0200

    ready to deploy

---
+++ src/main/java/converter/servlet/Converter.java
@@ -0,0 +1,59 @@
+package converter.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.zielinska.numbers.languages.Languages;
+import pl.zielinska.numbers.util.NumbersUtil;
+
+
+@WebServlet("/Converter")
+public class Converter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Converter() {
+        super();
+
+    }
+
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setCharacterEncoding("utf-8");
+    	long number = Long.parseLong(request.getParameter("number"));
+		String language = request.getParameter("language");
+		String text = "";
+		switch (language) {
+			case "British":
+				text = NumbersUtil.textValue(Languages.GB, number);
+				break;
+			case "American":
+				text = NumbersUtil.textValue(Languages.US, number);
+				break;
+			case "Polish":
+				text = NumbersUtil.textValue(Languages.PL, number);
+				break;
+			default:
+				text = "Select a language";
+		}
+		PrintWriter out = response.getWriter();		
+		out.println("<!DOCTYPE html>");
+		out.println("<html><body>");
+		out.println(text);
+		out.println("</html></body>");
+		/*
+		request.setAttribute("text_numb", text);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/form.jsp");
+		dispatcher.forward(request, response);*/
+
+
+	}
+
+}
---
+++ src/main/webapp/form.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"> 
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+		<title>Numbers to text converter</title>
+	</head>
+	<body>
+		<div id="heading" class="text-center text-white">
+			<form action="Converter" method="GET">
+				<h3>Number you want to convert to text: </h3>
+				<input type="text" class="form-control" placeholder="enter a number" name="number"/>
+				<br/><br/>
+				<h3>Select a language:</h3>
+				<select name="language" class="form-control">
+					<option>Polish</option>
+					<option>British</option>
+					<option>American</option>
+				</select>
+				<br/><br/>
+				<input type="submit" class="btn btn-light mt-5 btn-lg" value="submit">
+			</form>
+			<div id = "result"><p>${text_numb}<p></div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	</body>
+</html>
