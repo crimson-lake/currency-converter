561ba8acd689cf231aac1bac220ac5f43da7dc24
crimson-lake <sandra.zielinska@protonmail.com>
2019-08-08 12:11:31 +0200

    Rest Client to get exchange rates from NBP

--- src/main/java/rest/NBPClient.java
+++ src/main/java/rest/NBPClient.java
@@ -1,15 +1,26 @@
 package rest;
 
+import java.math.BigDecimal;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 public class NBPClient {
 	private final Client restClient = ClientBuilder.newClient();
-	private final WebTarget root = restClient.target("http://api.nbp.pl/api/exchangerates");
+	private final WebTarget root = restClient.target("http://api.nbp.pl/api/exchangerates/rates/A");
 	
-	public BigDecimal getExchangeRate() {
+	public BigDecimal getExchangeRate(String code) {
+		final WebTarget rate = root.path("/" + code);
 		
+		Invocation getRate = rate
+				.request()
+				.header("Accept", "application/json")
+				.buildGet();
+		
+		Response responseRate = getRate.invoke();
 	}
 
 }
